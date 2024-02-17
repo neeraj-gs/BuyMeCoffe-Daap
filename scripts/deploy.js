66fedc1560d510b4b13f229cbb299f1b6fa2e9a9
neeraj-gs <gsneeraj05@gmail.com>
2024-02-17 18:56:47 +0530

    Cleand up the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,13 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Chai = await hre.ethers.getContractFactory("Chai"); //fetching bytecode and ABI
+  const chai = await Chai.deploy(); //creating an instance of our smart contract, and no arguments to thte constructor
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await chai.deployed();//deploying your smart contract
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Deployed contract address:",`${chai.address}`); //fetching adress after teh contract is deployed
 }
 
 // We recommend this pattern to be able to use async/await everywhere
