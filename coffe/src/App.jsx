997b1853b3a42b1a7fb40999a83e75680fe59553
neeraj-gs <gsneeraj05@gmail.com>
2024-02-17 19:11:04 +0530

    Added code to connec tto metamsk and extract abia dn contract address

--- coffe/src/App.jsx
+++ coffe/src/App.jsx
@@ -1,6 +1,33 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 const App = () => {
+  const [state,setState]=useState({
+    provider:null,
+    signer:null,
+    contract:null,
+  })
+
+  useEffect(()=>{
+    const template = async()=>{
+      const contractAddress="";
+      const contractABI="";
+
+      //Connect ot metamask , for transactions on Goerli test net , Metamask consisys of infura api whoch actually help in connecting to blockhain
+      const {ethereum}=window; //fetch the ethereum instance provided by metamask from the window
+      const account = await ethereum.request({
+        method:"eth_requestAccounts" //automatically opens metamsk wallet when user visits the site
+        //metamsk is popped to connec to it
+      })
+    }
+    template();
+  },[])
+
+
   return (
-    <div className='text-3xl text-red-400'>App</div>
+    <div>
+
+    </div>
   )
 }
 
